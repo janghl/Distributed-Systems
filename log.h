3146eb2b602e0609c9a0e6af478db994d44d2963
Jeffrey Sun <js66@illinois.edu>
2023-09-10 16:39:55 -0500

    Share logging patterns across VMs

--- log.h
+++ log.h
@@ -16,6 +16,12 @@
 #include <random>
 
 inline void logging(std::string command, int line_number){
+		std::string pattern1 = "one-log";
+		std::string pattern2 = "some-logs";
+		std::string pattern3 = "all-logs";
+		std::string pattern4 = "rare-pattern";
+		std::string pattern5 = "somewhat-frequent-pattern";
+		std::string pattern6 = "frequent-pattern";
 		int machine = stoi(command);
 		std::ofstream log("machine"+std::to_string(machine)+".log");
 		if(!log.is_open())
@@ -31,45 +37,22 @@ inline void logging(std::string command, int line_number){
 		std::default_random_engine frequency_generator(frequency_rd());
 		std::uniform_int_distribution<int> frequency_distribution(0,1);
 
-		//generate a random pattern
-		std::string charset = "0123456789abcdefghijklmnopqrstuvwxyz";
-		std::random_device rd;
-		std::default_random_engine generator(rd());
-		std::uniform_int_distribution<int> distribution(0, charset.length() - 1);
-
 		while(count++ < line_number){
-
-			std::string sequence1;
-			std::string sequence2;
-			std::string sequence3;
-			std::string sequence4;
-			std::string sequence5;
-			std::string sequence6;
-
-			for(int length=0;length<10;length++){
-				sequence1+=charset[distribution(generator)];
-				sequence2+=charset[distribution(generator)];
-				sequence3+=charset[distribution(generator)];
-				sequence4+=charset[distribution(generator)];
-				sequence5+=charset[distribution(generator)];
-				sequence6+=charset[distribution(generator)];
-			}
- 
 			//generate lines that occur in one/some/all logs
 			if(machine==1)
-				log<<"occur in one log: "<<sequence1<<std::endl;
+				log<<"occur in one log: "<<pattern1<<std::endl;
 			if(machine%2==0)
-				log<<"occur in half logs: "<<sequence2<<std::endl;
-			log<<"occur in all logs: "<<sequence3<<std::endl;
+				log<<"occur in some logs: "<<pattern2<<std::endl;
+			log<<"occur in all logs: "<<pattern3<<std::endl;
 
 			//generate rare, somewhat frequent and frequent patterns
 			int frequency = frequency_distribution(frequency_generator);
 			if(frequency<rare)
-				log<<"rare pattern: "<<sequence4<<std::endl;
+				log<<"rare pattern: "<<pattern4<<std::endl;
 			if(frequency<somewhat_frequent)
-				log<<"somewhat frequent pattern: "<<sequence5<<std::endl;
+				log<<"somewhat frequent pattern: "<<pattern5<<std::endl;
 			if(frequency<frequent)
-				log<<"frequent pattern: "<<sequence6<<std::endl;
+				log<<"frequent pattern: "<<pattern6<<std::endl;
 		}
 		log.close();
 		return;
