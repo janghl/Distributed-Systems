42dc97747fb635812d902dba340ac52c37b143a9
janghl <janghl2001@gmail.com>
2023-09-23 13:39:03 -0500

    add T_cleanup and suspicion features

--- MP2/sender.h
+++ MP2/sender.h
@@ -7,11 +7,17 @@
 #include <string>
 #include <chrono>
 #include <thread>
+#include <mutex>
 #include <map>
 #include <vector>
+#define T_fail 30
+#define T_cleanup 20
+#define T_suspect 20
 #define heartbeat_interval 20   //heart rate
 #define Scale 10                //scale of machines
 enum Status {alive, failed, suspected};
+std::mutex mutex;
+bool suspicion = false;
 public struct MembershipList{
     std::string node_id = "\0";        //ip+port+timestamp
     int count = 0;
@@ -62,6 +68,7 @@ int sender(int id, struct MembershipList* list){
 */
 int send_heartbeats(int machine, struct MembershipList* list){
     while(true){
+        std::lock_guard<std::mutex> lock(mutex);
         list[machine].count++;
         list[machine].local_time++;
         srand (time(NULL));
@@ -79,23 +86,47 @@ int send_heartbeats(int machine, struct MembershipList* list){
     usage: local membership list, received membership list, machine number
 */
 int merge(struct MembershipList* list1, struct MembershipList* list2, int machine){
+    std::lock_guard<std::mutex> lock(mutex);
     for(int num=0; num<Scale && num!= machine; num++){
         if(list1[num].node_id=="\0" && list2[num].node_id!="\0"){
            list1[num].status = list2[num].status;
             list1[num].count = list2[num].count;
             list1[num].local_time = list1[machine].local_time;
             std::cout<< "create new entry "<<num<<std::endl;
-
         }
-        else if(list2[num].status!=alive && list1[num].status==alive){
+        if(suspicion == false){
+            if(list2[num].status!=alive && list1[num].status==alive){
         list1[num].status = list2[num].status;
         std::cout<< "machine "<<num<<" status changed to "<<list1[num].status<<std::endl;
         }
-        else if(list2[num].count > list1[num].count){
+            else if(list2[num].count > list1[num].count){
         list1[num].count = list2[num].count;
-        list1[num].local_time = list1[machine].local_time;
+        list1[num].local_time = list2[machine].local_time;
         std::cout<< "updated entry "<<num<<" on machine "<<machine<<std::endl;
         }
+        else
+        {
+            if(list2[num].status==failed || list1[num].status==failed){
+                list1[num].status = list2[num].status = failed;
+                std::cout<< "machine "<<num<<" has failed!"<<std::endl;
+            }
+            else
+            {
+                if(list2[num].count > list1[num].count){
+                    list1[num].count = list2[num].count;
+                    list1[num].local_time = list2[machine].local_time;
+                    std::cout<< "updated entry "<<num<<" on machine "<<machine<<std::endl;
+                    }
+                if(list2[num].status!=list1[num].status){       
+                    list1[num].status = list2[num].status;
+                    std::cout<< "machine "<<num<<" status changed to "<<list1[num].status<<std::endl;
+                }
+
+            }
+
+        }
+        }
+        
     }
     return 0;
 }
@@ -137,6 +168,22 @@ int receiver(int machine, struct MembershipList* list){
     return 0;
 }
 
+int checker(int machine, struct MembershipList* list){
+    for(int num=0; num<Scale && num!= machine; num++){
+        if(list[num].local_time-list[machine].local_time>=T_fail+T_cleanup){
+            if(suspicion==true){
+                list[num].status = suspected;
+                if(list[num].local_time-list[machine].local_time>=T_fail+T_cleanup+T_suspect)
+                list[num].status = failed;
+            }
+            else 
+                list[num].status = failed;
+        }
+    }
+    if(list[machine].status = suspected)
+        list[machine].status = alive;
+}
+
 int controller(int machine){
     struct MembershipList* list = new struct MembershipList[Scale];
 
@@ -152,6 +199,10 @@ int controller(int machine){
     std::thread receiver_thread{&receiver, this, machine, list};
     receiver_thread.detach();
     
+    // multithread checker
+    std::thread checker_thread{&checker, this, machine, list};
+    checker_thread.detach();
+
     return 0;
 
 
