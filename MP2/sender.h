0a64d7443315ea03d1484467b0e7540bac9e95a0
hj33 <hj33@fa23-cs425-6901.cs.illinois.edu>
2023-09-24 15:23:47 -0500

    In progress

--- MP2/sender.h
+++ MP2/sender.h
@@ -1,32 +1,175 @@
-#include <sys/socket.h>
-#include <unistd.h>
-#include <iostream>
+#include <algorithm>
+#include <chrono>
 #include <cstdlib>
 #include <ctime>
-#include <netdb.h>
-#include <string>
-#include <chrono>
-#include <thread>
-#include <mutex>
+#include <iostream>
 #include <map>
+#include <mutex>
+#include <netdb.h>
+#include <sstream>
+#include <string>
+#include <sys/socket.h>
+#include <thread>
+#include <tuple>
+#include <unistd.h>
+#include <unordered_map>
+#include <utility>
 #include <vector>
-#include "unit_test.h"
-#define T_fail 30
-#define T_cleanup 20
-#define T_suspect 20
-#define heartbeat_interval 20   //heart rate
-#define Scale 10                //scale of machines
 
-enum class Status {alive, failed, suspected}; // enum class
-std::mutex mutex; // shouldn't be in global scope and rename to be specific
-bool suspicion = false; // shouldn't be in global scope
-// 
-public struct MembershipEntry{ // named wrong MembershipEntry
-    std::string node_id = "\0";        //ip+port+timestamp, membership id can be struct
-    int count = 0;
-    int local_time = 0;
-    enum Status status = alive;
+//
+
+class Controller {
+public:
+  int controller(int machine) {
+    std::thread sender_thread(&Controller::SendHeartbeats, this);
+    sender_thread.detach();
+    std::thread receiver_thread(&Controller::Receiver, this);
+    receiver_thread.detach();
+    std::thread checker_thread(&Controller::Checker, this);
+    checker_thread.detach();
+    std::thread cli_thread(&Controller::Interface, this);
+    return 0;
+  }
+
+private:
+  void Log(const std::string& message) {
+    std::ofstream log_file;
+    log_file.open("mp2.log", std::ios::app);
+    // append to file new line
+  }
+  void Join() {
+    auto time = std::chrono::system_clock::now();
+    time_stamp_ = time.time_since_epoch().count();
+    NodeId node_id{host_, port_, time_stamp_};
+    list_mtx_.lock();
+    membership_list_.try_emplace(node_id, 0, 0, Status::kAlive);
+    list_mtx_.unlock();
+    // cond var to make sure the fd is connected
+    // then mutex lock around fd
+    // then mutex unlock
+    // send message to introducer to help join
+  }
+  void Leave() {
+    list_mtx_.lock();
+    auto it = membership_list_.find(NodeId{host_, port_, time_stamp_});
+    if (it != membership_list_.end()) {
+      Log("Couldn't find self entry in membership list");
+      return;
+    }
+    if (it->second.status != Status::kAlive) {
+      Log("Self status isn't alive");
+      return;
+    }
+    // send gossip to a few about leaving
+  }
+  void List() {
+    std::cout << "Membership List on host " + host_ << std::endl;
+    list_mtx_.lock();
+    for (const auto &[key, entry] : membership_list_) {
+      std::string status_string = status_to_string_[entry.status];
+      printf("Host: %s, count: %d, local_time: %d, status: %s",
+             key.host.c_str(), entry.count, entry.local_time,
+             status_string.c_str());
+      std::cout << std::endl;
+    }
+    list_mtx_.unlock();
+  }
+  void Monitor() {
+    std::cout << "Commands: join, leave, list" << std::endl;
+    std::cout << "Input: ";
+    std::string command;
+    while (std::cin >> command) {
+      if (command == "join") {
+        Join();
+      } else if (command == "leave") {
+        Leave();
+      } else if (command == "list") {
+        List();
+      } else {
+        std::cout << "Unidentified command, use one of join, leave, list"
+                  << std::endl;
+      }
+      std::cout << "Input: ";
+    }
+  }
+  std::string ListToString() const {
+    std::string retval;
+    std::stringstream ss(retval);
+    for (const auto &[key, value] : membership_list_) {
+      ss << key.host << "," << key.port << "," << key.time_stamp << "," << value.count
+         << "," << value.local_time << "," << status_to_string_.at(value.status)
+         << std::endl;
+    }
+    return retval;
+  }
+  enum class Status {
+    kAlive,
+    kFailed,
+    kSuspected,
+  };
+  std::unordered_map<std::string, Status> string_to_status_ = {
+      {"alive", Status::kAlive},
+      {"suspected", Status::kSuspected},
+      {"failed", Status::kFailed}};
+  std::unordered_map<Status, std::string> status_to_string_ = {
+      {Status::kAlive, "alive"},
+      {Status::kSuspected, "suspected"},
+      {Status::kFailed, "failed"}};
+  enum class Command {
+    kJoin,
+    kLeave,
+    kList,
+  };
+  struct NodeId {
+    std::string host;
+    std::string port;
+    long time_stamp;
+  };
+  struct MembershipEntry {
+    int count;
+    int local_time;
+    Status status;
+  };
+  std::map<NodeId, MembershipEntry> StringToList(const std::string &str) const {
+    std::map<NodeId, MembershipEntry> map;
+    std::string line;
+    while (std::getline(std::stringstream(str), line)) {
+      std::string substr;
+      std::vector<std::string> splitted;
+      while (getline(std::stringstream(line), substr, ',')) {
+        splitted.push_back(substr);
+      }
+      map.emplace(std::piecewise_construct,
+                  std::forward_as_tuple(splitted[0], splitted[1], std::stol(splitted[2])),
+                  std::forward_as_tuple(std::stoi(splitted[3]), std::stoi(splitted[4])), string_to_status_.at(splitted[5]));
+    }
+  }
+  const int kTFail = 30;
+  const int kTCleanup = 20;
+  const int kTSuspect = 20;
+  const int kHeartbeatInterval = 20;
+  const int kScale = 10;
+  std::mutex list_mtx_;
+  std::map<NodeId, MembershipEntry> membership_list_;
+  bool using_suspicion_;
+  int machine_;
+  std::string host_;
+  std::string port_;
+  long time_stamp_;
 };
+
+void delete() {
+  for (const auto &[key, value] : list1) {
+  key:
+    NodeId;
+  value:
+    MembershipEntry;
+    auto list2iterator = list2.find(key);
+    list2iterator->first : NodeId;
+    list2iterator->second : MembershipEntry;
+  }
+}
+
 const static char *hosts[] = {
     "fa23-cs425-6901.cs.illinois.edu", "fa23-cs425-6902.cs.illinois.edu",
     "fa23-cs425-6903.cs.illinois.edu", "fa23-cs425-6904.cs.illinois.edu",
@@ -40,6 +183,29 @@ const static char *hosts[] = {
     usage: target machine number, membership list
 */
 
+int sender(int id, struct MembershipList *list) {
+  int sock_fd = socket(AF_INET, SOCK_DREAM, IPPROTO_UDP);
+  struct addrinfo hints, *infoptr;
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_DREAM;
+  int addr = getaddrinfo(hosts[id], "8080", &hints, &infoptr);
+  if (addr) {
+    perror("sender cannot set address!");
+    return 1;
+  }
+  if (connect(sock_fd, infoptr->ai_addr, infoptr->ai_addrlen) == -1) {
+    perror("sender cannot connect! target machine number = " + id);
+    return 1;
+  }
+  char *buffer = new char[Scale * sizeof(struct MembershipList)];
+  std::memcpy(buffer, list, Scale * sizeof(struct MembershipList));
+  if (send(sock_fd, buffer, Scale * sizeof(struct MembershipList), 0) == -1) {
+    perror("sender cannot send to machine " + id);
+    return 1;
+  }
+  close(sock_fd);
+  return 0;
 int sender(int id, struct MembershipEntry* list){
     int sock_fd = socket(AF_INET, SOCK_DREAM, IPPROTO_UDP);
     struct addrinfo hints, *infoptr;
@@ -69,7 +235,7 @@ int sender(int id, struct MembershipEntry* list){
     generate and update my own heartbeats and local time
     send heartbeats to a random neighbour each 20ms
 */
-// change logic so that 
+// change logic so that
 // 2 3 4 5 6 7 8 9 10
 // 2 5 8 7 6 4 3 10 9
 // 2n - 1
@@ -93,55 +259,51 @@ int send_heartbeats(int machine, struct MembershipEntry* list){
 // 
 // TODO: specify merge in place, use const static
 /*  create neighbour's entry;
-    detect status change; 
+    detect status change;
     merging received list to local one;
     usage: local membership list, received membership list, machine number
 */
-int merge(struct MembershipEntry* list1, struct MembershipEntry* list2, int machine){
-    std::lock_guard<std::mutex> lock(mutex);
-    for(int num=0; num<Scale && num!= machine; num++){
-        if(list1[num].node_id=="\0" && list2[num].node_id!="\0"){
-           list1[num].status = list2[num].status;
-            list1[num].count = list2[num].count;
-            list1[num].local_time = list1[machine].local_time;
-            std::cout<< "create new entry "<<num<<std::endl;
-        }
-        else if(suspicion == false){
-            if(list2[num].status!=alive && list1[num].status==alive){
+int merge(struct MembershipList *list1, struct MembershipList *list2,
+          int machine) {
+  std::lock_guard<std::mutex> lock(mutex);
+  for (int num = 0; num < Scale && num != machine; num++) {
+    if (list1[num].node_id == "\0" && list2[num].node_id != "\0") {
+      list1[num].status = list2[num].status;
+      list1[num].count = list2[num].count;
+      list1[num].local_time = list1[machine].local_time;
+      std::cout << "create new entry " << num << std::endl;
+    }
+    if (suspicion == false) {
+      if (list2[num].status != alive && list1[num].status == alive) {
         list1[num].status = list2[num].status;
-        std::cout<< "machine "<<num<<" status changed to "<<list1[num].status<<std::endl;
-        }
-            else if(list2[num].count > list1[num].count){
+        std::cout << "machine " << num << " status changed to "
+                  << list1[num].status << std::endl;
+      } else if (list2[num].count > list1[num].count) {
         list1[num].count = list2[num].count;
         list1[num].local_time = list2[machine].local_time;
-        std::cout<< "updated entry "<<num<<" on machine "<<machine<<std::endl;
+        std::cout << "updated entry " << num << " on machine " << machine
+                  << std::endl;
+      } else {
+        if (list2[num].status == failed || list1[num].status == failed) {
+          list1[num].status = list2[num].status = failed;
+          std::cout << "machine " << num << " has failed!" << std::endl;
+        } else {
+          if (list2[num].count > list1[num].count) {
+            list1[num].count = list2[num].count;
+            list1[num].local_time = list2[machine].local_time;
+            std::cout << "updated entry " << num << " on machine " << machine
+                      << std::endl;
+          }
+          if (list2[num].status != list1[num].status) {
+            list1[num].status = list2[num].status;
+            std::cout << "machine " << num << " status changed to "
+                      << list1[num].status << std::endl;
+          }
         }
-        }
-        else
-        {
-            if(list2[num].status==failed || list1[num].status==failed){
-                list1[num].status = list2[num].status = failed;
-                std::cout<< "machine "<<num<<" has failed!"<<std::endl;
-            }
-            else
-            {
-                if(list2[num].count > list1[num].count){
-                    list1[num].count = list2[num].count;
-                    list1[num].local_time = list2[machine].local_time;
-                    std::cout<< "updated entry "<<num<<" on machine "<<machine<<std::endl;
-                    }
-                if(list2[num].status!=list1[num].status){       
-                    list1[num].status = list2[num].status;
-                    std::cout<< "machine "<<num<<" status changed to "<<list1[num].status<<std::endl;
-                }
-
-            }
-
-        
-        }
-        
+      }
     }
-    return 0;
+  }
+  return 0;
 }
 
 
